ec10c44d1b0c76cfc1a516b2d39229a6cc81e5ba
YuNanlong <yunanlong@126.com>
2017-09-07 22:14:57 +0800

    添加v0.2

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 用于爬取浙江大学校内论坛CC98->休闲娱乐->mm图片板块的帖子中的妹子图片。
 
+### v0.2
+
+除了不需要selenium和PhantomJS之外，其他运行环境的配置与v0.1一致。
+
+这个版本利用发送cookie绕过登录，直接获取mm图片板块帖子的页面源码。
+
+命令行执行
+
+```shell
+python3 cc98-mmpic-scraper-v0.2.py
+```
+
+在程序运行时根据提示进行输入，需要访问mm图片板块后通过浏览器的开发者工具获取登录后请求中的cookie：
+
+![](https://ws1.sinaimg.cn/large/006tKfTcgy1fjbct1m50rj31kw0vsnjc.jpg)
+
+### v0.1
+
 需要selenium、requests和beautifulsoup4这三个第三方库和PhantomJS这个第三方工具。本爬虫脚本编写时使用的是python3.5.1，selenium3.4.3，requests2.18.2，beautifulsoup4 4.6.0，PhantomJS2.1.1。运行环境是MacBook Pro (Retina, 13-inch, Early 2015)， macOS Sierra10.12.6 (16G29)。如果在其他环境中运行可能需要更改源代码第10行的路径。运行前需要连接ZJUWLAN并在源代码中填写自己的账号名和密码。
 
 selenium下载
@@ -31,8 +49,9 @@ brew install phantomjs
 命令行执行
 
 ```
-python3 cc98.py
+python3 cc98-mmpic-scraper-v0.1.py
 ```
 由于本人技术水平有限，尝试使用requests的post方法发送HTTP header和form data来绕过CC98论坛的登录，但是依然无法获取登录后的界面，所以使用selenium结合PhantomJS来模拟浏览器进行爬取，同时为了避免爬取过程中某些页面请求时间过长导致PhantomJS与CC98服务器的连接中断，在爬取过程中仅保存图片的URL，在爬取结束后再调用requests库的get方法下载URL。
 
 如果有绕过登录的其他方法，欢迎提issue。
+
---
+++ cc98-mmpic-scraper-v0.2.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+import requests
+import bs4
+import os
+import re
+
+class MmPicScraper:
+    headers = {
+        'Host': 'www.cc98.org',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate',
+        'Cookie': 'cc98Simple=0; BoardList=BoardID=Show; owaenabled=True; autoplay=True; ASPSESSIONIDQQBBTQBT=KOMHBHAAFBHLKFEGNGEJPEJI; upNum=0; aspsky=username=singledog&usercookies=3&userid=546532&useranony=&userhidden=2&password=89611f8806cc39a2',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Cache-Control': 'max-age=0'
+    }
+    post_url = 'http://www.cc98.org/list.asp?'
+    root_url = 'http://www.cc98.org/'
+    file_url = 'http://file.cc98.org/uploadfile/'
+
+    boardid = '146'
+
+    def __init__(self, cookie, total_page):
+        MmPicScraper.headers['Cookie'] = cookie
+        self.total_page = total_page
+        self.post_link_list = []
+        self.img_link_list = []
+
+    def get_post_link(self):
+        for i in range(self.total_page):
+            data = {'boardid': self.boardid, 'page': str(i + 1), 'action': ''}
+            result = requests.get(self.post_url, 'html.parser', headers=self.headers, data=data)
+            result.raise_for_status()
+            result_soup = bs4.BeautifulSoup(result.text)
+            post_link_list = result_soup.select('a[id^="topic_"]')
+            self.post_link_list.extend(post_link_list)
+        print('前%d页的帖子链接抓取完毕' % self.total_page)
+
+    def get_img_link(self):
+        for post_link in self.post_link_list:
+            post = requests.get(self.root_url + post_link.attrs['href'], 'html.parser', headers=self.headers)
+            post.raise_for_status()
+            post_soup = bs4.BeautifulSoup(post.text)
+            ubbcode1_text = post_soup.select('#ubbcode1')[0].get_text()
+            img_link_list = re.compile(r'http://file.cc98.org/uploadfile(.+?)\[/upload\]').findall(ubbcode1_text)
+            self.img_link_list.extend(img_link_list)
+        print('前%d页的帖子中的图片链接抓取完毕' % self.total_page)
+
+    def download_img(self):
+        for img_link in self.img_link_list:
+            img_url = self.file_url + img_link
+            img_src = requests.get(img_url)
+            try:
+                img_src.raise_for_status()
+            except Exception as exc:
+                print('There was a problem: %s' % (exc))
+                continue
+            img_file = open(os.path.join('mmPic1', os.path.basename(img_url)), 'wb')
+            for chunk in img_src.iter_content(100000):
+                img_file.write(chunk)
+            img_file.close()
+        print('前%d页的帖子中的图片下载完毕' % self.total_page)
+
+os.chdir('/Users/yunanlong/Desktop')
+os.makedirs('mmPic1', exist_ok=True)
+print('输入cookie')
+cookie = input()
+print('输入爬取页数')
+total_page = input()
+scraper = MmPicScraper(cookie, int(total_page))
+scraper.get_post_link()
+scraper.get_img_link()
+scraper.download_img()
